f14c1938b40da50276d3e9cc3bc06e74ab8f3be5
Naveen Ramanjulu <55444343+nram76@users.noreply.github.com>
2023-05-26 12:40:41 -0500

    input form complete

---
+++ index.ts
@@ -0,0 +1,12 @@
+
+
+export class Functions {
+    public reset(){
+     let email = document?.getElementById("email");
+     let subject = document?.getElementById("subject");
+     let message = document?.getElementById("message");
+            email = null;
+            subject = null;
+            message = null;
+    }
+}
